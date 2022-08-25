cea9bfaae7041f573bedf475c4d7aec17bceef22
peterclo <pierre.schoenhenz@gmail.com>
2022-08-25 18:19:23 +0200

    ✨ (Whole app) : Create a layout with an app bar and a drawer

--- app.vue
+++ app.vue
@@ -140,23 +140,7 @@ const items = ref([
 </script>
 
 <template>
-  <!--<NuxtLayout>-->
-  <v-layout>
-    <v-app-bar color="primary" prominent>
-      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Magic Words</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn variant="text" icon="mdi-login"></v-btn>
-      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" bottom temporary>
-      <v-list :items="items"></v-list>
-    </v-navigation-drawer>
-
+  <NuxtLayout :drawer="drawer" :items="items">
     <v-main>
       <display-word
         :french-word="frenchWord"
@@ -165,7 +149,6 @@ const items = ref([
         :german-sentence="germanSentence"
       ></display-word>
     </v-main>
-  </v-layout>
-  <!--<NuxtPage />-->
-  <!--</NuxtLayout>-->
+    <!--<NuxtPage />-->
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps(["drawer", "items"]);
+</script>
+
+<template>
+  <div>
+    <v-layout>
+      <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Magic Words</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-login"></v-btn>
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" bottom temporary>
+        <v-list :items="items"></v-list>
+      </v-navigation-drawer>
+
+      <slot />
+    </v-layout>
+  </div>
+</template>
